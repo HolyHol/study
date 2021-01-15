30144cfcb6560e52a2e91f96ffb46163aa3603b1
Rena <nenavigygygl@gmail.com>
2021-01-15 16:46:46 +0100

    Update replace, fixes style

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,46 +1,20 @@
 module Exercise
   module Arrays
     class << self
-      def max(list)
-        return nil if list.empty?
-      
-        if list.length == 1
-          list[0] # base case
-        else
-          sub_max = max(list[1..-1])
-          list[0] > sub_max ? list[0] : sub_max
+      def max(array)
+        return array if array.empty?
+
+        max_element = array[0]
+        array.each do |current_element|
+          max_element = current_element if max_element < current_element
         end
+        max_element
       end
 
-      def max(array)
-        return nil if array.empty?
-        
-        if array.length == 1
-          array[0] # base case
-        else
-          sub_max = max(array[1..-1])
-          array[0] > sub_max ? array[0] : sub_max
-        end
-      end
-      
-      def replaces_positive_array(array, max_array_new)
-        
-        return [] if array.empty?
-        
-        sub_array = replaces_positive_array(array[1..-1], max_array_new)
-        
-        if array[0] < 0
-          sub_array.unshift(array[0])
-          
-        else 
-          sub_array.unshift(max_array_new) 
-          
-        end
-      end
-      
       def replace(array)
-        max_array_new = max(array)
-        replaces_positive_array(array, max_array_new)
+        array.map do |current_element|
+          current_element.positive? ? max(array) : current_element
+        end
       end
 
       def search(_array, _query)
@@ -48,22 +22,19 @@ module Exercise
         # Функция должна возвращать индекс элемента
         low = 0
         high = _array.length - 1
-
         while low <= high
           mid = (low + high) / 2
           guess = _array[mid]
-        
-          if guess == _query
-            return mid 
-          
-          elsif guess > _query
+
+          return mid if guess == _query
+
+          if guess > _query
             high = mid - 1
-          
-          else 
-            low = mid + 1
+
+          elsif (low = mid + 1)
           end
         end
-      return -1
+        -1
       end
     end
   end
